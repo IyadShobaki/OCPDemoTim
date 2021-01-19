8e92b242aef5263bba626e84867fc0d2ad7348ab
IyadShobaki <iyadshobaki82@gmail.com>
2021-01-19 13:49:41 -0500

    Using Delegates Instead Of Interfaces
    To create a variety of employees accounts. I tried to use delgates to acheive the same result  as using interfaces.  (applying OCP )

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleUI.ApplicantsFolder;
+using ConsoleUI.UsingDelegates;
 using System;
 using System.Collections.Generic;
 
@@ -8,27 +9,100 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            List<IApplicant> applicants = new List<IApplicant>()
+            //List<IApplicant> applicants = new List<IApplicant>()
+            //{
+            //    new Executive { FirstName = "Tim", LastName = "Corey" },
+            //    new Manager { FirstName = "Sue", LastName = "Storm" },
+            //    new Applicant { FirstName = "Mike", LastName = "Smith" }
+            //};
+
+            //List<Employee> employees = new List<Employee>();
+
+            //foreach (var a in applicants)
+            //{
+            //    employees.Add(a.AccountProcessor.Create(a));
+            //}
+
+            //foreach (var e in employees)
+            //{
+            //    Console.WriteLine($"{e.FirstName} {e.LastName}, Email: {e.Email} IsManager: {e.IsManager} IsExecutive {e.IsExecutive}");
+            //}
+
+            AccountsModel model = new AccountsModel();
+            var applicant = new Applicant()
             {
-                new Executive { FirstName = "Tim", LastName = "Corey" },
-                new Manager { FirstName = "Sue", LastName = "Storm" },
-                new Applicant { FirstName = "Mike", LastName = "Smith" }
+                FirstName = "Iyad",
+                LastName = "Shobaki"
             };
 
-            List<Employee> employees = new List<Employee>();
-
-            foreach (var a in applicants)
+            var manager = new Applicant()
             {
-                employees.Add(a.AccountProcessor.Create(a));
-            }
+                FirstName = "Mike",
+                LastName = "Smith"
+            };
+            var executive = new Applicant()
+            {
+                FirstName = "Sue",
+                LastName = "Storm"
+            };
+            var basicEmployee = model.Create(applicant, GetBasicEmployee);
+            var managerEmployee = model.Create(manager, GetManagerEmployee);
+            var executiveEmployee = model.Create(executive, GetExecutiveEmployee);
+
+            List<Employee> employees = new List<Employee>
+            {
+                basicEmployee,
+                managerEmployee,
+                executiveEmployee
+            };
+
 
             foreach (var e in employees)
             {
-                Console.WriteLine($"{e.FirstName} - {e.LastName}, Email: {e.Email} IsManager: {e.IsManager} IsExecutive {e.IsExecutive}");
+                Console.WriteLine($"{e.FirstName} {e.LastName}, Email: {e.Email} IsManager: {e.IsManager} IsExecutive {e.IsExecutive}");
             }
 
-
             Console.ReadKey();
+        }
+
+        public static Employee GetBasicEmployee(Applicant applicant)
+        {
+            Employee employee = new Employee
+            {
+                FirstName = applicant.FirstName,
+                LastName = applicant.LastName,
+                Email = $"{applicant.FirstName.Substring(0, 1)}{applicant.LastName}@acme.com"
+            };
+
+            return employee;
+        }
+
+        public static Employee GetManagerEmployee(Applicant applicant)
+        {
+            Employee employee = new Employee
+            {
+                FirstName = applicant.FirstName,
+                LastName = applicant.LastName,
+                Email = $"{applicant.FirstName.Substring(0, 1)}{applicant.LastName}@acmecorp.com",
+
+                IsManager = true
+            };
+
+            return employee;
+        }
+        public static Employee GetExecutiveEmployee(Applicant applicant)
+        {
+            Employee employee = new Employee
+            {
+                FirstName = applicant.FirstName,
+                LastName = applicant.LastName,
+                Email = $"{applicant.FirstName}.{applicant.LastName}@acmecorp.com",
+
+                IsManager = true,
+                IsExecutive = true
+            };
+
+            return employee;
         }
     }
 }
---
+++ ConsoleUI/UsingDelegates/AccountsModel.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleUI.ApplicantsFolder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI.UsingDelegates
+{
+    public class AccountsModel
+    {
+        public Employee Create(Applicant applicant, 
+            Func<Applicant, Employee> handleAccount)
+        {
+            return handleAccount(applicant);
+        }
+    }
+}
